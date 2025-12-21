a42d0c145155d8371895560cec588a9cf097c823
Noor <noorii4906@gmail.com>
2025-12-21 16:34:50 +0500

    ufff

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,8 @@
-module.exports = connectDB;
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // 'localhost' ya '127.0.0.1' ko hata kar 'mongodb-service' likhein
         const mongoURI = 'mongodb://mongodb-service:27017/my-todo-app';
-        
         await mongoose.connect(mongoURI);
         console.log('MongoDB Connected Successfully...');
     } catch (err) {
--- server/index.js
+++ server/index.js
@@ -4,17 +4,13 @@ const cors = require('cors');
 
 const app = express();
 
-// 1. Connect Database
 connectDB();
 
-// 2. Middleware
-app.use(express.json({ extended: false })); // Body Parser
-app.use(cors()); // Allow Frontend to connect
+app.use(express.json({ extended: false }));
+app.use(cors());
 
-// 3. Define Routes
-app.use('/api', require('./routes/auth'));  // Auth routes (Login/Register)
-app.use('/api/todos', require('./routes/todos')); // Todo routes
+app.use('/api', require('./routes/auth'));
+app.use('/api/todos', require('./routes/todos'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
